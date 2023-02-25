97d492b310e779ca20cd79081d5af34241880168
unknown <RorySVelthuis@gmail.com>
2023-02-25 14:48:55 +1300

    created app

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let title = "Svelte App";
+    console.log(title);
+
+    export let count = 0;
+</script>
+
+<h1>Welcome to {title}</h1>
+
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<button on:click={() => count++}>Click</button>
+<p>{count}</p>
